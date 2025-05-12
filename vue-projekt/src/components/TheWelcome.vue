79a26bfb1976ebda8eb037f81a19c54d5e8c6d28
Mert <mertiset11@gmail.com>
2025-05-12 11:57:03 +0200

    Fehleranpassung

--- vue-projekt/src/components/TheWelcome.vue
+++ vue-projekt/src/components/TheWelcome.vue
@@ -5,7 +5,6 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
-import IconFilm from "@/components/icons/IconFilm.vue";
 
 const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 interface WatchlistEntry {
